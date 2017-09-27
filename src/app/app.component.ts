96533396d4767ead912ab8e44cb0da3cced88d8d
David Ramirez <davidsramirez11@gmail.com>
2017-09-27 09:35:35 -0700

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "application",
+	templateUrl: "./app.html"
+})
+export class AppComponent {
+	// write your component code here
+}
