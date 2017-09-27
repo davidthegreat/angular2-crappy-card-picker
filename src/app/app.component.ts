1e5eeacd00ea94f644743cf5cb345da6cdc17133
David Ramirez <davidsramirez11@gmail.com>
2017-09-27 10:12:14 -0700

    add pick a card functuallity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,37 @@
 import { Component } from "@angular/core";
+import Deck from "./../classes/deck";
+
 
 @Component({
 	selector: "application",
-	templateUrl: "./app.html"
+	templateUrl: "./app.html",
+	styleUrls: ["./app.css"]
 })
 export class AppComponent {
 	// write your component code here
+
+	public deck:Deck = new Deck()
+
+	public cardsDrawn = [];
+
+
+	public getDeckCardCount() {
+		return this.deck.getCardCount();
+	}
+
+	lifeBarHeight(){
+		const baseHeight = 46
+		return this.deck.getCardCount() / 52 * baseHeight;
+	}
+
+
+	public DrawCard() {
+		this.cardsDrawn.push(this.deck.drawCard());
+	}
+
+	public return(card) {
+		this.deck.returnCardToDeck(card);
+		this.cardsDrawn.splice(this.cardsDrawn.indexOf(card), 1);
+
+	}
 }
